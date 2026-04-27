c2e21ce55b1389f84e33a003f559ebbcc3b61e25
Qeete <qeettee@mail.ru>
2026-04-27 22:49:23 +0400

    прогнулся под женщин

--- minigame2/coin.gd
+++ minigame2/coin.gd
@@ -2,9 +2,11 @@ extends Area2D
 
 @onready var game_manager: Node = %GameManager
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var clue_dialog = $"/root/Game/UI/ClueDialog"
 
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	game_manager.add_points()
+	clue_dialog.visible = true
+	get_tree().paused = true
 	queue_free()
-	
--- minigame2/scenes/clue_dialog.gd
+++ minigame2/scenes/clue_dialog.gd
@@ -1,15 +1,10 @@
-# exit_dialog.gd
 extends Panel
 
 func _ready():
-	$YesButton.pressed.connect(_on_yes)
-	$NoButton.pressed.connect(_on_no)
+	$ContinueButton.pressed.connect(_on_continue)
 
-func _on_no():
+func _on_continue():
 	visible = false
-	get_tree().paused = false
-
-func _on_yes():
 	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	get_tree().change_scene_to_file("res://levels/level.tscn")
